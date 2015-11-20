fd9067142cca19b95f18fc7e8721488009f17151
Guilherme <guilherme.guic@gmail.com>
2015-11-19 23:21:10 -0200

    prod.py remove

--- encontreaqui/settings/base.py
+++ encontreaqui/settings/base.py
@@ -93,21 +93,10 @@ WSGI_APPLICATION = 'encontreaqui.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': 'XP4XE0HeBGgD',
-        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
